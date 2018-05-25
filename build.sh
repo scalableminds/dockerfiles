610544f97bd891ea41599a46e02347d7b1dfbe4f
Jonathan Striebel <jonathan.striebel@gmail.com>
2018-05-25 18:17:39 +0200

    build.sh: delay resolving of

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 EXTRA_TAGS=""
 if [[ "$CI" == "true" ]]; then
-	EXTRA_TAGS="$EXTRA_TAGS -t scalableminds/$APP:${CIRCLE_BRANCH}__${CIRCLE_BUILD_NUM}"
-	EXTRA_TAGS="$EXTRA_TAGS -t scalableminds/$APP:${CIRCLE_BRANCH}"
+	EXTRA_TAGS="$EXTRA_TAGS -t scalableminds/\$APP:${CIRCLE_BRANCH}__${CIRCLE_BUILD_NUM}"
+	EXTRA_TAGS="$EXTRA_TAGS -t scalableminds/\$APP:${CIRCLE_BRANCH}"
 fi
 
 for APP in $(./get_valid_order.sh); do
