25941895c48b3867a802ec87ba00813d4a3e579a
Jonathan Striebel <jonathan.striebel@gmail.com>
2018-05-25 18:10:46 +0200

    extract image building into a script

---
+++ build.sh
@@ -0,0 +1,14 @@
+EXTRA_TAGS=""
+if [[ "$CI" == "true" ]]; then
+	EXTRA_TAGS="$EXTRA_TAGS -t scalableminds/$APP:${CIRCLE_BRANCH}__${CIRCLE_BUILD_NUM}"
+	EXTRA_TAGS="$EXTRA_TAGS -t scalableminds/$APP:${CIRCLE_BRANCH}"
+fi
+
+for APP in $(./get_valid_order.sh); do
+  cd $APP
+  docker build \
+  	$EXTRA_TAGS \
+    -t scalableminds/$APP \
+    .
+  cd ..
+done
