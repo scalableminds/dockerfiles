97650988974cd1f683fb1e698e65fcbee7f0beb1
Jonathan Striebel <jonathan.striebel@gmail.com>
2018-05-25 18:19:32 +0200

    fix build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
-EXTRA_TAGS=""
-if [[ "$CI" == "true" ]]; then
-	EXTRA_TAGS="$EXTRA_TAGS -t scalableminds/\$APP:${CIRCLE_BRANCH}__${CIRCLE_BUILD_NUM}"
-	EXTRA_TAGS="$EXTRA_TAGS -t scalableminds/\$APP:${CIRCLE_BRANCH}"
-fi
+#!/usr/bin/env bash
+set -Eeuo pipefail
 
 for APP in $(./get_valid_order.sh); do
   cd $APP
+	EXTRA_TAGS=""
+	if [[ "$CI" == "true" ]]; then
+		EXTRA_TAGS="$EXTRA_TAGS -t scalableminds/$APP:${CIRCLE_BRANCH}__${CIRCLE_BUILD_NUM}"
+		EXTRA_TAGS="$EXTRA_TAGS -t scalableminds/$APP:${CIRCLE_BRANCH}"
+	fi
   docker build \
   	$EXTRA_TAGS \
     -t scalableminds/$APP \
