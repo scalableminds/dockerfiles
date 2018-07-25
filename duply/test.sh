2f27cec773711f0cc1aaa7d4309a634edd955892
Jonathan Striebel <jonathan.striebel@gmail.com>
2018-07-25 13:43:18 +0200

    make duply test circleci compliant

--- duply/test.sh
+++ duply/test.sh
@@ -8,54 +8,50 @@ TEST_HOSTPATH="/backup_here"
 TEST_USER="none"
 TEST_PASSWORD="none"
 
-# create temp dir
-TMPDIR="$(mktemp -d)"
-if [[ ! "$TMPDIR" || ! -d "$TMPDIR" ]]; then
-  echo "Could not create temp dir"
-  exit 1
-fi
+# make tester container to use mounts on CircleCI
+docker run \
+  --name tester \
+  -d \
+  -v "/to_backup" \
+  -v "$TEST_HOSTPATH" \
+  -v "/restore_here" \
+  --rm \
+  debian:buster sleep infinity
 
-# deletes the temp directory on exit
+# deletes the tester container on exit
 function cleanup {      
-  rm -rf "$TMPDIR"
+  docker kill tester
 }
 trap cleanup EXIT
 
 # create dummy data
-mkdir "$TMPDIR"/to_backup "$TMPDIR"/backup_here "$TMPDIR"/tmp
-echo "Backup Me!" > "$TMPDIR"/to_backup/file_to_backup.txt
+docker exec tester ls
+docker exec tester bash -c 'echo "Backup Me!" > /to_backup/file_to_backup.txt'
 
 # backup
 docker run \
-    -v "$TMPDIR/to_backup:/to_backup" \
-    -v "$TMPDIR/backup_here:$TEST_HOSTPATH" \
-    -v "$TMPDIR/tmp:/tmp" \
-    -e "GPG_PW=$TEST_GPG_PW" \
-    -e "SCHEME=$TEST_SCHEME" \
-    -e "HOST=$TEST_HOST" \
-    -e "HOSTPATH=$TEST_HOSTPATH" \
-    -e "USER=$TEST_USER" \
-    -e "PASSWORD=$TEST_PASSWORD" \
-    --rm \
-    -ti \
-    scalableminds/duply
+  --volumes-from tester \
+  -e "GPG_PW=$TEST_GPG_PW" \
+  -e "SCHEME=$TEST_SCHEME" \
+  -e "HOST=$TEST_HOST" \
+  -e "HOSTPATH=$TEST_HOSTPATH" \
+  -e "USER=$TEST_USER" \
+  -e "PASSWORD=$TEST_PASSWORD" \
+  --rm \
+  scalableminds/duply
 
 # restore
-mkdir "$TMPDIR"/restore_here
 docker run \
-    -v "$TMPDIR/restore_here:/restore_here" \
-    -v "$TMPDIR/backup_here:$TEST_HOSTPATH" \
-    -v "$TMPDIR/tmp:/tmp" \
-    -e "GPG_PW=$TEST_GPG_PW" \
-    -e "SCHEME=$TEST_SCHEME" \
-    -e "HOST=$TEST_HOST" \
-    -e "HOSTPATH=$TEST_HOSTPATH" \
-    -e "USER=$TEST_USER" \
-    -e "PASSWORD=$TEST_PASSWORD" \
-    -ti \
-    --rm \
-    scalableminds/duply restore /restore_here
+  --volumes-from tester \
+  -e "GPG_PW=$TEST_GPG_PW" \
+  -e "SCHEME=$TEST_SCHEME" \
+  -e "HOST=$TEST_HOST" \
+  -e "HOSTPATH=$TEST_HOSTPATH" \
+  -e "USER=$TEST_USER" \
+  -e "PASSWORD=$TEST_PASSWORD" \
+  --rm \
+  scalableminds/duply restore /restore_here
 
 # check recovered data
-diff -r "$TMPDIR"/to_backup "$TMPDIR"/restore_here
-cat "$TMPDIR"/restore_here/file_to_backup.txt
+docker exec tester diff -r /to_backup /restore_here
+docker exec tester cat /restore_here/file_to_backup.txt
