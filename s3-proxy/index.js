a83f302b42175c41d7255faef8e6adac5862001a
Norman Rzepka <norman.rzepka@scalableminds.com>
2021-06-23 16:15:51 +0200

    adds a simple S3 proxy (#22)
    * adds a simple S3 proxy
    
    * readme
    
    * readme

---
+++ s3-proxy/index.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const morgan = require("morgan");
+const AWS = require("aws-sdk");
+
+const S3_BUCKET = process.env.S3_BUCKET;
+const S3_BUCKET_PREFIX = process.env.S3_BUCKET_PREFIX || "";
+const PORT = parseInt(process.env.PORT || "3000", 10);
+
+const s3Client = new AWS.S3();
+
+async function tryReadFile(filepath, res) {
+  try {
+    const obj = await s3Client
+      .getObject({
+        Bucket: S3_BUCKET,
+        Key: `${S3_BUCKET_PREFIX}${filepath}`,
+      })
+      .promise();
+
+    if (obj.ContentType != null) {
+      res.set("Content-Type", obj.ContentType);
+    }
+    if (obj.ContentLength != null) {
+      res.set("Content-Length", obj.ContentLength);
+    }
+    if (obj.ContentEncoding != null) {
+      res.set("Content-Encoding", obj.ContentEncoding);
+    }
+    if (obj.ContentDisposition != null) {
+      res.set("Content-Disposition", obj.ContentDisposition);
+    }
+    if (obj.ETag != null) {
+      res.set("ETag", obj.ETag);
+    }
+    if (obj.Expires != null) {
+      res.set("Expires", obj.Expires);
+    }
+    if (obj.CacheControl != null) {
+      res.set("Cache-Control", obj.CacheControl);
+    }
+    if (obj.WebsiteRedirectLocation != null) {
+      res.set("Location", obj.WebsiteRedirectLocation);
+    }
+    res.send(obj.Body);
+
+    return true;
+  } catch (err) {
+    console.error(err);
+    if (err.code === "NoSuchKey") {
+      return false;
+    }
+    throw err;
+  }
+}
+
+const app = express();
+app.use(morgan("combined"));
+
+app.get("/health", (req, res) => {
+  res.end("Ok");
+});
+app.get("*", async (req, res) => {
+  try {
+    const justPath = decodeURI(req.path.replace(/^\//, ""));
+    if (await tryReadFile(justPath, res)) {
+      return;
+    }
+    if (await tryReadFile(`${justPath}/index.html`, res)) {
+      return;
+    }
+    res.status(404).end();
+  } catch (err) {
+    console.error(err);
+    res.status(500).end();
+  }
+});
+
+app.listen(PORT, "0.0.0.0");
