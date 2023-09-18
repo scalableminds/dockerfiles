0bed92995b4a971532504473f2ce5adbfba471cd
frcroth <frcroth@gmail.com>
2023-09-18 09:42:35 +0200

    Add meta feed

--- om-bistro/openmensa_server.py
+++ om-bistro/openmensa_server.py
@@ -1,6 +1,6 @@
 import datetime
 
-from flask import Flask, make_response
+from flask import Flask, make_response, Response
 
 from pdf_downloader import download_pdf
 from pdf_parser import parse_pdf
@@ -8,6 +8,7 @@ from pdf_parser import parse_pdf
 app = Flask(__name__)
 
 last_download = datetime.date.fromtimestamp(0)
+
 
 @app.route("/feed")
 def feed():
@@ -17,6 +18,27 @@ def feed():
         download_pdf()
         last_download = datetime.date.today()
     return parse_pdf()
+
+
+@app.route("/meta")
+def meta():
+    xml = """
+        <openmensa version="2.1"
+           xmlns="http://openmensa.org/open-mensa-v2"
+           xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+           xsi:schemaLocation="http://openmensa.org/open-mensa-v2 http://openmensa.org/open-mensa-v2.xsd">
+    <canteen>
+    <name>Schraders Bistro</name>
+    <address>August-Bebel-Straße 26–53 14482 Potsdam</address>
+    <availability>public</availability>
+    <feed name="full">
+        <url>https://openmensa.scm.io/feed</url>
+        <schedule dayOfMonth="*" dayOfWeek="1" hour="8"/>
+    </feed>
+    </canteen>
+    </openmensa>
+    """
+    return Response(xml, mimetype="text/xml")
 
 
 @app.route("/health_check")
